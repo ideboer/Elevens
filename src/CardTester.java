9c93451e6d983cd6333d85347f4503bbc051278d
ideboer <isabella@songdeboer.net>
2020-03-24 09:38:48 -0600

    finished answers

--- src/CardTester.java
+++ src/CardTester.java
@@ -9,5 +9,27 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("ace", "clubs", 1);
+	
+		Card card2 = new Card("ace", "clubs", 1);
+		Card card3 = new Card("3", "spades", 3);
+
+		System.out.println(card1.suit());
+		System.out.println(card1.rank());
+		System.out.println(card1.pointValue());
+		System.out.println(card1.matches(card2));
+		System.out.println(card1.toString());
+
+		System.out.println(card2.suit());
+		System.out.println(card2.rank());
+		System.out.println(card2.pointValue());
+		System.out.println(card2.matches(card3));
+		System.out.println(card2.toString());
+
+		System.out.println(card3.suit());
+		System.out.println(card3.rank());
+		System.out.println(card3.pointValue());
+		System.out.println(card3.matches(card1));
+		System.out.println(card3.toString());
 	}
 }
