d7c46c8d1504d0ddd4e10d857db00e97953818e7
ideboer <54374876+ideboer@users.noreply.github.com>
2020-03-24 09:56:36 -0600

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -74,7 +74,7 @@ public class Card {
 	 */
 	public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-		if (otherCard.pointValue() == pointValue && otherCard.rank().equals(rank) && otherCard.suit().equals(suit)) {
+		if (otherCard.pointValue() == pointValue && otherCard.rank().equals(this.rank()) && otherCard.suit().equals(this.suit())) {
 			return true;
 		}
 		return false;
